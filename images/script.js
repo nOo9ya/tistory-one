858a9e273dd34944d70fd13264a1e8c45ad435c3
nOo9ya <noo9ya@noo9ya.com>
2017-10-19 19:41:58 +0900

    Section 11 Article

---
+++ images/script.js
@@ -0,0 +1,78 @@
+;(function($) {
+
+    var Area = {};
+
+    Area.SocialShare = (function() {
+
+        var SOCIAL_SHARE = {
+            kakaostory: {
+                url: "https://story.kakao.com/share",
+                width: 640,
+                height: 400,
+                makeShareUrl: function(url) {
+                    return this.url + "?url=" + encodeURIComponent(url);
+                }
+            },
+            facebook: {
+                url: "https://www.facebook.com/sharer/sharer.php",
+                width: 640,
+                height: 400,
+                makeShareUrl: function(url) {
+                    return this.url + "?display=popup&u=" + encodeURIComponent(url);
+                }
+            },
+            twitter: {
+                url: "https://twitter.com/intent/tweet",
+                width: 640,
+                height: 400,
+                makeShareUrl: function(url) {
+                    return this.url + "?url=" + encodeURIComponent(url);
+                }
+            }
+        };
+
+        var onClick = function(e) {
+            var $this = $(this),
+                service = SOCIAL_SHARE[$this.data("service")],
+                url = location.href;
+
+            if (service) {
+                e.preventDefault();
+                window.open(service.makeShareUrl(url), "", "width=" + service.width + ", height=" + service.height);
+            }
+        };
+
+        $(".article-share").on("click", "a", onClick);
+
+    })();
+
+    $.Area = Area;
+
+    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed, iframe[src^='http://www.youtube.com'], iframe[src^='https://www.youtube.com'], iframe[src^='http://videofarm.daum.net'], iframe[src^='https://videofarm.daum.net'], iframe[src^='//videofarm.daum.net'], iframe[src^='//tv.kakao.com'], iframe[src^='//kakaotv.daum.net']"),
+        $fluidEl = $(".area_view");
+
+    $allVideos.each(function() {
+
+        $(this)
+            // jQuery .data does not work on object/embed elements
+            .attr('data-aspectRatio', this.height / this.width)
+            .removeAttr('height')
+            .removeAttr('width');
+
+    });
+
+    $(window).resize(function() {
+
+        var newWidth = $fluidEl.width();
+        $allVideos.each(function() {
+
+            var $el = $(this);
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.attr('data-aspectRatio'));
+
+        });
+
+    }).resize();
+
+})(jQuery);
